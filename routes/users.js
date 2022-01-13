28c75c0cf8feeac73db9a7e251f4953f7b27eeea
HeleneSoubeyrand <helenesoubi@gmail.com>
2022-01-13 17:32:33 +0100

    routes user OK

--- routes/users.js
+++ routes/users.js
@@ -5,34 +5,68 @@ const User = require("../models/User")
 const Tweet = require("../models/Tweet")
 const Comment = require("../models/Comment")
 
+
 // Créer un user => POST (C de CRUD pour CREATE)
 
 app.post('/', async (req, res) => {
-    const { tweet } = req.body
-    const { comment } = req.body
   
     try {
-      const user = await new User({ ...req.body })
-      
-      user.save(async (err, user) => {
-        
-        if (user) {
-            const getTweet = await Tweet.findById(tweet)
-            getTweet.users.push(user._id)
-            getTweet.save()
-    
-            res.json(user)
-            return
-          }
-    
-          console.log(err)
-          res.status(500).json({ error: err })
-        })
-
+      const user = new User({ ...req.body })
+      const userInsert = await user.save()
+      res.json(userInsert)    
     } catch (err) {
       console.log(err)
       res.status(500).json({ error: err })
     }
-  })
+})
+
+
+// Récupérer tous les users 
+
+app.get('/', async (req, res) => {
+  
+  try {
+    const users = await User.find({}).exec()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
+})
+
+
+// Récupérer un user 
+
+app.get('/:username', async (req, res) => {
+  const { username } = req.params
+  
+  try {
+    const user = await User.findOne({ username : username }).exec()
+    res.json(user)
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
+})
+
+
+// Modifier un user
+
+app.put('/:username', async (req, res) => {
+  const { username } = req.params
+  
+  try {
+    const user = await User.findOneAndUpdate(
+      { username : username  },
+      { ...req.body },
+      { new: true }
+    ).exec()
+
+    res.json(user)
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err })
+  }
+})
+
 
 module.exports = app
