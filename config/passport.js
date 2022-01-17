f405b8a28b58f5a8246a11250414aaa3d47ad2bc
Edouard-Germain <edangermain@gmail.com>
2022-01-17 19:16:52 +0100

    wtf

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,37 @@
 const passport = require("passport")
 const LocalStrategy = require('passport-local').Strategy;
-
+const mongoose = require("mongoose")
 const User = require("../models/User")
 
 
 // Local Strategy 
 passport.use(new LocalStrategy(User.authenticate()))
 
-// passport.use(new LocalStrategy(
-//     async (email, password, done) => {
+passport.use(new LocalStrategy(
+    async (email, password, done) => {
        
-//             try {
-//                 // Find user with email
-//                 const user = await User.findOne({ email: email }) 
-//                 if (!user) {
-//                     return done(null, false, 
-//                         { message: 'No user by that email' });
-//                 } else {
-//                  // Match with the password
-//                 }
+            try {
+                // Find user with email
+                const user = await User.findOne({ email: email }) 
+                if (!user) {
+                    return done(null, false, 
+                        { message: 'No user by that email' });
+                } 
+                return done(null,user)
                    
-//             } catch (err) {
-//                 console.log(err)
-//                 res.status(500).json({ error: err })
-//             }
-//     })
-// )
+            } catch (err) {
+                console.log(err)
+                res.status(500).json({ error: err })
+            }
+    })
+)
 
 
 // lors de l'authentification
 // sérialise l'instance utilisateur avec les informations transmises 
 // et la stocke dans la session via un cookie
 passport.serializeUser((user, done) => {
-    done(null, user.id)
+    done(null, user._id)
 })
 
 // à chaque demande ultérieure de désérialisation de l'instance, 
